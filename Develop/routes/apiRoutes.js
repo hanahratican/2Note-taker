9b4536911b6011d97fe5fa90595cd000f641fa9e
Hanah R <hanahratican@gmail.com>
2023-06-07 21:52:44 -0700

    Added code from last attempt into this new repo. Still running into the same error.

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const db = require('./db/db.json');
+const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+    const savedNotes = db;
+    res.json(savedNotes);
+}
+);
+
+router.post('/notes', (req, res) => {
+    const newNote = req.body;
+    const savedNotes = db;
+    newNote.id = db.length.toString();
+    savedNotes.push(newNote);
+    console.log(savedNotes);
+    fs.writeFileSync(path.join(__dirname,'./db/db.json'), JSON.stringify(savedNotes), (err) => {
+        if (err) throw err;
+        res.json(savedNotes);
+    });
+}
+);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const db = require('./db/db.json');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    const savedNotes = db;
+    res.json(savedNotes);
+});
+
+app.post('/api/notes', (req, res) => {
+    const savedNotes = db;
+    const newNote = req.body;
+    savedNotes.push(newNote);
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(savedNotes), (err) => {
+        if (err) throw err;
+        res.json(savedNotes);
+    });
+});
+
+
+    
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+    }
+);
